27434f7525aa94a69a6a04acbbb89d7c970f2913
Manujapabodhana <rpmanuja123@gmail.com>
2025-11-23 19:16:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from 'express';
 
 const app = express();
  app.get("/",(req,res)=>{
+
+
     // console.log(req);
     console.log("Get request recieved");
     res.send("Hello from the server");
