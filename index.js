2f0323fcc904d385b353672233351b17fb9a99c3
Manujapabodhana <rpmanuja123@gmail.com>
2025-11-24 01:28:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 import express from 'express';
 
 const app = express();
+
+app.use(express.json());
  
 app.get("/", (req,res) => {
-    console.log("hello world")
+    console.log(req.body);
+    console.log("Get request received");
+
+    let prefix="Mr"
+    if(req.body.gender == "female"){
+        prefix="Ms"
+    }
+    
 
     res.json(
     {
-        message: "hello world"
+        message: "hello "+ prefix + " " + req.body.name
     }
 )
 });
