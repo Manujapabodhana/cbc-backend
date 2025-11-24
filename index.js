5267fa3ff04233919db1e2cc462703f72c7e006b
Manujapabodhana <rpmanuja123@gmail.com>
2025-11-24 21:47:31 +0530

    a

--- index.js
+++ index.js
@@ -1,29 +1,52 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
 app.use(express.json());
- 
-app.get("/", (req,res) => {
-    console.log(req.body);
-    console.log("Get request received");
 
-    let prefix="Mr"
-    if(req.body.gender == "female"){
-        prefix="Ms"
-    }
-    
+// Load connection string from .env
+const connectionString = process.env.MONGO_URI;
 
-    res.json(
-    {
-        message: "hello "+ prefix + " " + req.body.name
-    }
-)
+mongoose
+  .connect(connectionString)
+  .then(() => {
+    console.log("connected to mongodb");
+  })
+  .catch(() => {
+    console.log("error connecting to mongodb");
+  });
+
+const studentSchema = new mongoose.Schema(
+  {
+    name: String,
+    age: Number,
+    city: String
+  }
+);
+
+app.get("/", (req, res) => {
+  console.log(req.body);
+  console.log("Get request received");
+
+  let prefix = "Mr";
+  if (req.body.gender === "female") {
+    prefix = "Ms";
+  }
+
+  res.json({
+    message: "hello " + prefix + " " + req.body.name,
+  });
 });
 
-
+app.post("/", (req, res) => {
+    console.log(req.body);
+    
+});
 
 app.listen(5000, () => {
-    console.log("server is started");
-}
- ) 
+  console.log("server is started");
+});
